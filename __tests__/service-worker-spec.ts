cf1929ee812809880016ada6a2825dee641c4da8
stevenpack <steven.pack.code@gmail.com>
2018-03-22 22:45:14 -0700

    initial commit. wip

---
+++ __tests__/service-worker-spec.ts
@@ -0,0 +1,38 @@
+import { ApiRacer, IHttpResponder, GdaxSpotProvider, SpotPrice } from '../src/service-worker';
+import { Request } from 'node-fetch';
+import { Response } from 'node-fetch';
+
+const racer = new ApiRacer();
+
+class DelayedResponder implements IHttpResponder {
+  constructor(private delay: number, private response: string) {}
+
+  getResponse(req: Request): Promise<Response> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(new Response(this.response));
+      }, this.delay);
+    });
+  }
+}
+
+test('fastest wins', async () => {
+  let responders = [
+    new DelayedResponder(10, 'fast'),
+    new DelayedResponder(100, 'slow'),
+  ];
+  let req = new Request('btc-usd');
+  let res = await racer.race(req, responders);
+  console.log(`winner: ${res.body}`);
+  expect(res.body).toEqual('fast');
+});
+
+test('INT: gdax spot', async() => {
+  let gdax = new GdaxSpotProvider();
+  let res = await gdax.getResponse(new Request("http://somerequest/abc"))
+  expect(res).not.toBeNull();
+  expect(res.body).not.toBeNull();
+  let spot: SpotPrice = await res.json();
+  console.log(`${JSON.stringify(spot)}`);
+  expect(spot.code).toEqual("btc-usd");
+}); 
---
+++ src/service-worker.ts
@@ -0,0 +1,101 @@
+import fetch from 'node-fetch';
+import { Request } from 'node-fetch';
+import { Response } from 'node-fetch';
+///===========///
+
+export class ApiRacer {
+  race(req: Request, responders: IHttpResponder[]): Promise<Response> {
+    let arr = responders.map(r => r.getResponse(req));
+    return Promise.race(arr);
+  }
+}
+
+export interface IHttpResponder {
+  getResponse(req: Request): Promise<Response>;
+}
+
+export interface ICryptoSpotApi {
+  getSpot(code: string): Promise<SpotPrice>;
+}
+
+export class SpotPrice {
+  constructor(public code: string, public price: string, public utcTime: string, ) {    
+  }
+}
+
+/**
+ * Returns a spot price from GDAX.
+ * 
+ * GDAX response looks like this:
+ * {
+ *  "trade_id":40240431,
+ *  "price":"8371.58000000",
+ *  "size":"0.01668154",
+ *  "bid":"8371.57",
+ *  "ask":"8371.58",
+ *  "volume":"17210.40916422",
+ *  "time":"2018-03-23T05:23:59.807000Z"
+ * }
+ */
+export class GdaxSpotProvider implements ICryptoSpotApi, IHttpResponder {
+  async getResponse(req: Request): Promise<Response> {
+    
+    let code = "btc-usd"; req.url;//TODO: extract
+    let spot = await this.getSpot(code)
+    
+    return new Response(JSON.stringify(spot));
+  }
+
+  public async getSpot(code: string): Promise<SpotPrice> {
+    let res = await fetch(`https://api.gdax.com/products/${code}/ticker`);
+    return this.parseSpot(code, res);
+  }
+
+private async parseSpot(code: string, res: Response): Promise<SpotPrice> {
+    let json: any = await res.json();
+    return new SpotPrice(code, json.price, json.time)
+  }
+}
+
+export class BitfinexSpotProvider implements ICryptoSpotApi, IHttpResponder {
+
+  getResponse(req: Request): Promise<Response> {
+    throw new Error("Method not implemented.");
+  }
+  getSpot(code: string): Promise<SpotPrice> {
+    throw new Error("Method not implemented.");
+  }
+}
+
+// let a = new CoinbaseProvider();
+// let spot = a.getSpot('BTC-USD');
+
+// window.addEventListener("fetch", event => {
+//   event.respondWith(fetchAndReplace(event.request))
+// })
+
+// async function fetchAndReplace(request: Request) {
+//   // Fetch from origin server.
+//   let response = await fetch(request)
+
+//   // Make sure we only modify text, not images.
+//   let type = response.headers.get("Content-Type") || ""
+//   if (!type.startsWith("text/")) {
+//     // Not text. Don't modify.
+//     return response
+//   }
+
+//   // Read response body.
+//   let text = await response.text()
+
+//   // Modify it.
+//   let modified = text.replace(
+//       /Worker/g, "Minion")
+
+//   // Return modified response.
+//   return new Response(modified, {
+//     status: response.status,
+//     statusText: response.statusText,
+//     headers: response.headers
+//   })
+// }
